71b66f336a66649c41bc71395edaad545290c35d
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-22 18:06:18 -0400

    steps 1-4, 6

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -15,7 +15,6 @@ function findLongestWord(arr) {
     return null;
   }
   let longestWord = '';
-
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].length > longestWord.length) {
       longestWord = arr[i];
